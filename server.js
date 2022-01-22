3a3086233c5584e78b310c6af15e161e11ea0883
Michael Newman <therealmichaelnewman@gmail.com>
2022-01-22 17:54:12 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -26,8 +26,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Express serve listening on port ${PORT}`);
