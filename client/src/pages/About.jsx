ff9dfe21c955bb340fb27ee568e6bf4dc09f7929
Michael Newman <therealmichaelnewman@gmail.com>
2022-01-29 15:30:51 -0500

    update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
             <h2>What's a graphic novel? What's the difference between a graphic novel and comic book?</h2>
             <p>Generally speaking, a graphic novel is one standalone story created with comics content. Comic books are usually serialized with stories that don't have a set completion. But there are great variations to this!</p>
 
-            <h2>Hey, I can't find Superman! Where's Flaming Carrot?!? Why can't I find the specific graphic novel I'm looking for?!?!</h2>
+            <h2>Hey, I can't find Superman!!! Where's Flaming Carrot?!?! Why can't I find the specific graphic novel I'm looking for?!?!</h2>
             <p>I'm building this by hand! It's a labor of love! I won't have every comic and graphic novel ever made, but I hope to have a robust and exciting collection!</p>
         </div>
     )
